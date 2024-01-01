142b243f5e9084e5893dbc2a0dea2f18de8a6d99
Saad Srabon <saadsrabon2@gmail.com>
2024-01-02 03:09:10 +0600

    add thunk api done

---
+++ src/redux/features/transactions/transactionsSlice.js
@@ -0,0 +1,39 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { addTransaction } from "./transactionsApi";
+
+// this is a slice of the store
+
+// add a slice for transactions
+export const addTransactionThunk =createAsyncThunk('transaction/addTransaction', async (payload) => {
+    const data = await addTransaction(payload);
+    return data;
+});
+
+
+
+const transactionsSlice = createSlice({
+    name: "transactions",
+    initialState: {
+        transactions: [],
+        isLoading: false,
+        error: null,
+    },
+    extraReducers:builder=>{
+        builder.addCase(addTransactionThunk.pending, (state, action) => {
+            state.isLoading = true;
+            state.error = null;      
+        });
+        builder.addCase(addTransactionThunk.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.transactions.push(action.payload);
+        });
+        builder.addCase(addTransactionThunk.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.error.message;
+            state.transactions = [];
+        });
+    }
+});
+
+
+export default transactionsSlice.reducer;
